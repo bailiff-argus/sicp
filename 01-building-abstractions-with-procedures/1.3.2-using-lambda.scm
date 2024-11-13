e1714d63e8ba748439a47d31822055eb22e8a8c5
David Martynenko <dmartynenko64@gmail.com>
2024-11-13 23:04:40 +0300

    fix: 1.3.2 error in let usage

--- 01-building-abstractions-with-procedures/1.3.2-using-lambda.scm
+++ 01-building-abstractions-with-procedures/1.3.2-using-lambda.scm
@@ -46,10 +46,10 @@
 ;; this approach is aliased with 'let'
 (define (f x y)
   (let ((a (+ 1 (* x y)))
-        (b (- 1 y)))
-  (+ (* x (square a))
-     (* y b)
-     (* a b)))
+        (b (- 1 y))
+    (+ (* x (square a))
+       (* y b)
+       (* a b))))
 
 ;; let is bound locally
 ;; variable values are computed outside of let
